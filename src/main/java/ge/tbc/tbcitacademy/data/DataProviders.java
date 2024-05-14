ba686cc12c586cdcec843d5e8a606df98698e2b9
user2 <kostaareshide@gmail.com>
2024-05-14 20:29:10 +0400

    second commit

---
+++ src/main/java/ge/tbc/tbcitacademy/data/DataProviders.java
@@ -0,0 +1,20 @@
+package ge.tbc.tbcitacademy.data;
+
+import application.api.AuthenticationApi;
+import com.github.javafaker.Faker;
+import org.testng.annotations.DataProvider;
+
+public class DataProviders {
+    Faker faker = new Faker();
+    @DataProvider
+    public Object[][] getEmailAndPassword() {
+        return new Object[][] {
+                {faker.internet().emailAddress(), faker.internet().password()},
+                {faker.internet().emailAddress(), faker.internet().password()},
+                {faker.internet().emailAddress(), faker.internet().password()},
+                {faker.internet().emailAddress(), faker.internet().password()},
+                {faker.internet().emailAddress(), Constants.password + faker.internet().password()}
+
+        };
+    }
+}
